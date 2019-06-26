660117314c0af76eecaaed8f2ca4e389bbefa488
Kristian F Jones <me@kristianjones.xyz>
2019-06-26 10:36:09 -0500

    fix: Fix stuff

--- src/lib/Migrate.ts
+++ src/lib/Migrate.ts
@@ -6,8 +6,12 @@ type VMDirection = 'Original' | 'Temporary';
 
 let Direction: VMDirection = process.env.MODE as VMDirection;
 
-
-export const migrateVMStorage = async (ps: Shell, { originalStorage, tempStorage }: VMsType) => {
+/**
+ * 
+ * @param ps PowerShell Shell Instance
+ * @param VM VMs.json VM entry
+ */
+export const migrateVMStorage = async (ps: Shell, { originalStorage, tempStorage, name }: VMsType) => {
   const dataStore = Direction === 'Original' ? originalStorage : tempStorage;
   console.log(`Migrating ${name} to ${dataStore}`);
   await run(ps, `Get-VM "${name}" | Move-VM -Datastore "${dataStore}"`);
